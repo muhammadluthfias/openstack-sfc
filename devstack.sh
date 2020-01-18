501b73c4f0c763be2fe2f76eb8c0dcd4be517d7e
luthfi <ashafingilutfi@gmail.com>
2020-01-18 23:09:06 +0700

    update devstack.sh

--- devstack.sh
+++ devstack.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 echo "Install OpenStack devstack Requirements"
-echo ""
+echo "========================================"
 
 sudo apt update
 sudo apt install git -y
 sudo useradd -s /bin/bash -d /opt/stack -m stack
 echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
-sudo su - stack
-cd ~
-git clone https://opendev.org/openstack/devstack -b stable/stein
 
-devstack="~/devstack"
+sudo -u stack -H sh -c "cd ~;git clone https://opendev.org/openstack/devstack -b stable/stein"
+
+devstack="/opt/stack/devstack"
 if [ -e $devstack ]; then
 	echo "Installation Success"
 else 
 	echo "Installation not Success"
 fi 
+
+sudo su - stack
